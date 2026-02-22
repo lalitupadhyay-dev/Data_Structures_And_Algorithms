754e992932eeeeb28ca1c84fb145671ca08ef272
lalitupadhyay-dev <ulalit498@gmail.com>
2026-02-22 11:23:56 +0530

    Starting the Git repo for DSA

---
+++ with_java/src/Arrays/Wave1.java
@@ -0,0 +1,74 @@
+package Arrays;
+
+/*
+Problem statement:
+You are given a 2D array of size n×m containing integers.
+Your task is to print all the elements of the matrix row by row in wave form:
+Print the first row from left to right, the second row from right to left,
+and so on...
+
+Input:
+The first line contains two integers n and m (1 ≤ n,m ≤ 1000).
+
+The next n lines each contain m integers, the elements of the matrix.
+All elements satisfy 0 ≤ ai,j ≤ 105.
+
+Output:
+Print the expected wave form, as shown in the example.
+
+Examples:
+I/P:
+3 3
+1 2 3
+4 5 6
+7 8 9
+
+O/P:
+1 2 3 6 5 4 7 8 9
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Wave1 {
+
+    public static void solve (int[][] arr, StringBuilder sb) {
+        for (int i = 0; i < arr.length; ++i) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < arr[i].length; ++j) {
+                    sb.append(arr[i][j]).append(" ");
+                }
+            } else {
+                for (int j = arr[i].length - 1; j >= 0; --j) {
+                    sb.append(arr[i][j]).append(" ");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n, m;
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; ++i) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; ++j) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        solve(arr, sb);
+
+        System.out.print(sb);
+
+    }
+}
